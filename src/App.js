765ed185201253062455ff0816520fc01392c6dc
orvizz <153991700+orvizz@users.noreply.github.com>
2024-06-23 21:41:31 +0200

    first block

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let name = "mario"
+  let surname = "orviz"
+  let validUser = true
+  let countries = ["Sapin","Portugal","Germany"]
+  let objectList = [{ name: "orbs", surname: "orbi" }, { name: "noes", surname: "kane"}, {name: "kaps", surname: "kabs"}]
+  let exampleJSX = <h1>ejemplo</h1>
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        { validUser === true ? 
+          <ul>
+            <li>{name}</li>
+            <li>{surname}</li>
+          </ul>
+          : <p>No info</p>
+        }
+        {exampleJSX}
+        { countries.map(e => <li>{e}</li>) }
+        { objectList.map( ob => <div>
+          <h3>{ob.name}</h3>
+          <p>{ob.surname}</p>
+        </div>)}
+        <p className="testStyle">Para</p>
+      </div>
+    </>
   );
 }
 
