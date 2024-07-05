13f7d850d0031170e80ba253c4362c7efc3854e5
orvizz <153991700+orvizz@users.noreply.github.com>
2024-07-05 13:34:10 +0200

    7.2 entrada de datos finnished

--- src/App.js
+++ src/App.js
@@ -1,30 +1,46 @@
 import './App.css';
+import {useState, useRef, useEffect} from "react"
 
 function App() {
-  let name = "mario"
-  let surname = "orviz"
-  let validUser = true
-  let countries = ["Sapin","Portugal","Germany"]
-  let objectList = [{ name: "orbs", surname: "orbi" }, { name: "noes", surname: "kane"}, {name: "kaps", surname: "kabs"}]
-  let exampleJSX = <h1>ejemplo</h1>
+  let [name, setName] = useState();
+
+  let [num1, setNum1] = useState(0);
+  let [num2, setNum2] = useState(0);
+  let [total, setTotal] = useState(0);
+
+  useEffect(() => {
+    setTotal(num1+num2)
+  }, [num1, num2])
+
+  let onChangeInput = (e) => {
+    setName(e.currentTarget.value)
+    console.log("onChangeInput " + e.currentTarget.value)
+  }
+
+  let onChangeNumber1 = (e) => {
+    setNum1(parseFloat(e.currentTarget.value))
+  }
+
+  let onChangeNumber2 = (e) => {
+    setNum2(parseFloat(e.currentTarget.value))
+  }
+
+  let onClickCalculate= () => {
+    setTotal(num1+num2)
+  }
 
   return (
     <>
       <div className="App">
-        { validUser === true ? 
-          <ul>
-            <li>{name}</li>
-            <li>{surname}</li>
-          </ul>
-          : <p>No info</p>
-        }
-        {exampleJSX}
-        { countries.map(e => <li>{e}</li>) }
-        { objectList.map( ob => <div>
-          <h3>{ob.name}</h3>
-          <p>{ob.surname}</p>
-        </div>)}
-        <p className="testStyle">Para</p>
+        <h2>Data input 1</h2>
+        <input type="text" placeholder='Your name here' onChange={onChangeInput}/>
+        <p>my name: {name}</p>
+
+        <h2>Calculator</h2>
+        <input type="number" onChange={onChangeNumber1}></input>
+        <input type="number" onChange={onChangeNumber2}></input>
+        <button onClick={onClickCalculate}>Calculate</button>
+        <p>Total {num1} + {num2}: {total}</p>
       </div>
     </>
   );
