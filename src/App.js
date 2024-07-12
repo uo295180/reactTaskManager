2229dc57aef0ef3677b45deab7914385b0f4a3d6
orvizz <153991700+orvizz@users.noreply.github.com>
2024-07-12 12:40:31 +0200

    DetailsOfTaskComponent created and page styled

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import './App.css';
 import {useState, useRef, useEffect} from "react"
+import DetailsOfTaskComponent from './Components/DetailsOfTaskComponent';
 
 function App() {
   let [name,setName] = useState("");
   let [place, setPlace] = useState("");
   let [priority, setPriority] = useState(0);
 
-  let nameRef = useRef("");
-  let placeRef = useRef("");
-  let priorityRef = useRef(0);
-
-  let formRef = useRef();
 
   let [tasks, setTasks] = useState([
     {
@@ -32,60 +28,33 @@ function App() {
       priority: priority
     }
     setTasks([...tasks, newTask])
+    setName("");
+    setPlace("");
+    setPriority("");
   }
 
-  let addTaskWithRefs = () => {
-    let newTask = {
-      name: nameRef.current.value,
-      place: placeRef.current.value,
-      priority: priorityRef.current.value
-    }
-    setTasks([...tasks, newTask])
-    nameRef.current.value = "";
-    placeRef.current.value = "";
-    priorityRef.current.value = "";
-  }
-
-  let addTaskWithFormRef = () => {
-    let formData = new FormData(formRef.current);
-    let newTask = {
-      name: formData.get("name"),
-      place: formData.get("place"),
-      priority: formData.get("priority")
-    }
-    setTasks([...tasks, newTask])
-  }
-
+  
   return (
-      <div>
-        <h2>Add task</h2>
-        <input type="text" placeholder="name"></input>
-        <input type="text" placeholder="place" ></input>
-        <input type="number" placeholder="priority"></input>
-        <button onClick={addTask}> Add task </button>
-
-        <h2>Add tasks Ref</h2>
-        <input ref={nameRef} type="text" placeholder="name" onChange={(e) => { setName(e.currentTarget.value) }}></input>
-        <input ref={placeRef} type="text" placeholder="place" onChange={(e) => { setPlace(e.currentTarget.value) }}></input>
-        <input ref={priorityRef} type="number" placeholder="priority" onChange={(e) => { setPriority(parseInt(e.currentTarget.value)) }}></input>
-        <button onClick={addTaskWithRefs}> Add task </button>
-
-        <h2>Add tasks Form Ref</h2>
-        <form ref={formRef}>
-          <input name="name" type="text" placeholder="name" onChange={(e) => { setName(e.currentTarget.value) }}></input>
-          <input name="place" type="text" placeholder="place" onChange={(e) => { setPlace(e.currentTarget.value) }}></input>
-          <input name="priority" type="number" placeholder="priority" onChange={(e) => { setPriority(parseInt(e.currentTarget.value)) }}></input>
-        </form>
-        <button onClick={addTaskWithFormRef}> Add task </button>
-        
-        <ul>
+      <div className="container">
+        <h1>List of Tasks</h1>
+        <ul className='task-list'>
           { tasks.map( t => 
-          <li>
-            <b>{t.name}</b>
-            <div>Priority: {t.priority}</div>
-            <div>{t.place}</div>
+          <li key={t.name}>
+            {t.name}
           </li>) }
         </ul>
+
+        <div className='task-form'>
+        <h2>Add task</h2>
+          <input type="text" placeholder="name" value={name} onChange={(e) => {setName(e.currentTarget.value)}}></input>
+          <input type="text" placeholder="place" value={place} onChange={(e) => {setPlace(e.currentTarget.value)}}></input>
+          <input type="number" placeholder="priority" value={priority} onChange={(e) => {setPriority(e.currentTarget.value)}}></input>
+          <button onClick={addTask}> Add task </button>
+        </div>
+        
+        <DetailsOfTaskComponent />
+        
+
       </div>
   );
 }
---
+++ src/Components/DetailsOfTaskComponent.js
@@ -0,0 +1,14 @@
+
+
+let DetailsOfTaskComponent = () => {
+    return (
+        <div className="task-details">
+            <h2>Details of Task</h2>
+            <p>Name: </p>
+            <p>Place: </p>
+            <p>Priority: </p>
+        </div>
+    )
+}
+
+export default DetailsOfTaskComponent
